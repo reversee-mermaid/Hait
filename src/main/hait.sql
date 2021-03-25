b651884463b758ba303be2c96a10727013fd8578
kny3705 <skdus3023@gmail.com>
2021-03-25 16:33:35 +0900

    fix sql

--- src/main/hait.sql
+++ src/main/hait.sql
@@ -1,18 +1,23 @@
+drop table t_admin;
 drop table t_city;
 drop table t_application;
 drop table t_owner;
 drop table t_rstrnt;
 drop table t_customer;
 drop table t_reserv;
-drop table t_author;
 
+select * from t_admin;
 select * from t_city;
 select * from t_application;
 select * from t_owner;
 select * from t_rstrnt;
 select * from t_customer;
 select * from t_reserv;
-select * from t_author;
+
+CREATE TABLE t_admin (
+	id varchar(5) not null unique,
+	pw varchar(100) not null
+);
 
 CREATE TABLE t_city (
   pk int unsigned NOT NULL,
@@ -35,57 +40,42 @@ CREATE TABLE t_application (
 	CONSTRAINT CHECK(process_status IN (-1, 0, 1))
 );
 
-
 CREATE TABLE t_owner (
 	pk int unsigned auto_increment primary key,
 	nm varchar(50) not null,
 	pw varchar(100) not null,
 	email varchar(100) not null unique,
 	contact varchar(20) not null unique,
-	regdate datetime default now(),
-    enabled boolean default false
+	regdate datetime default now()
 );
 
 CREATE TABLE t_rstrnt (
 	pk int unsigned auto_increment primary key,
 	owner_pk int unsigned,
 	nm varchar(50) not null,
-	contact varchar(20) not null unique,
-	location varchar(100) not null unique,
+	contact varchar(20) unique,
+	location varchar(100) unique,
 	city_pk int unsigned NOT NULL,
-	more_info varchar(2000) not null,
-	porfile_img varchar(200) unique,
-	state int default 0,
+	more_info varchar(2000),
+	profile_img varchar(200) unique,
+	state int default -1,
+	enabled boolean default false,
 	regdate datetime default now(),
 	FOREIGN KEY (owner_pk) REFERENCES t_owner(pk),
 	FOREIGN KEY (city_pk) REFERENCES t_city(pk),
 	constraint check(state in(-1, 0, 1))
 );
 
-CREATE TABLE t_customer (
-	pk int unsigned auto_increment primary key,
-	contact varchar(20) not null unique,
-	regdate datetime default now()
-);
-
 CREATE TABLE t_reserv (
 	pk int unsigned auto_increment primary key,
 	seq int unsigned not null,
 	rstrnt_pk int unsigned,
-	customer_pk int unsigned,
+	contact varchar(20) not null,
 	headcount int unsigned not null,
 	process_status int default 0,
 	regdate datetime default now(),
 	FOREIGN KEY (rstrnt_pk) REFERENCES t_rstrnt(pk),
-	FOREIGN KEY (customer_pk) REFERENCES t_customer(pk),
 	constraint check (process_status IN (-3, -2, -1, 0, 1, 2))
-);
-
-create table t_author (
-	owner_email varchar(100) not null,
-    author varchar(20) not null, 
-    foreign key (owner_email) references t_owner(email),
-    constraint check (author in('ROLE_ADMIN', 'ROLE_OWNER'))
 );
 
 INSERT INTO t_city 
